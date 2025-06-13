8cfa5fa4d9212f63ba33b715e1d60cbd2e3387b6
ivyan26733 <ds26733@gmail.com>
2025-06-13 19:48:37 +0530

    rabbitmq working

--- enrollment-service/enrollment-service/src/main/java/com/LearnSphere/enrollment_service/listner/HandleUserDelete.java
+++ enrollment-service/enrollment-service/src/main/java/com/LearnSphere/enrollment_service/listner/HandleUserDelete.java
@@ -1,19 +1,29 @@
 package com.LearnSphere.enrollment_service.listner;
 
+import com.LearnSphere.enrollment_service.config.RabbitMQConfig;
 import com.LearnSphere.enrollment_service.dto.UserDeleteEvent;
 import com.LearnSphere.enrollment_service.repo.EnrollmentRepo;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Component
 public class HandleUserDelete {
 
     @Autowired
     private EnrollmentRepo enrollmentRepo;
 
-    @RabbitListener(queues = "user.delete.queue")
+    @Transactional
+    @RabbitListener(queues = RabbitMQConfig.ENROLLMENT_SERVICE_QUEUE)
     public void handleUserDelete(UserDeleteEvent event){
-        String email = event.getEmail();
-        System.out.println("Deleting user with email: "+ email);
-        enrollmentRepo.deleteByStudentEmail(email);
+        try {
+            System.out.println("🟢 [Listener Triggered] Deleting user with email: " + event.getEmail());
+            enrollmentRepo.deleteByStudentEmail(event.getEmail()); // Or enrollmentRepo
+            System.out.println("✅ [Deleted] User: " + event.getEmail());
+        } catch (Exception ex) {
+            System.out.println("❌ [Error] Exception in listener: " + ex.getMessage());
+            ex.printStackTrace(); // Most important!
+        }
     }
 }
